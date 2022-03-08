076bae2c3585bbd666b6614cada24ba3f3d742b2
Kevinoo5 <87882945+Kevinoo5@users.noreply.github.com>
2022-03-08 11:55:17 +0200

    Add files via upload
    Lisasime järjendid, kuhu panna nimetus, hind ja lisainfo

--- AastaTegija.py
+++ AastaTegija.py
@@ -1,9 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.request import urlopen
+import json
+
 
 url = "http://192.168.22.172/menu-example/"
 sisu = requests.get(url).text
 doc = BeautifulSoup(sisu, "html.parser")
-pealkirjad = doc.select('strong')
+toidud = []
+toit = {}
+lisainfod = []
+hinnad = []
+pealkirjad = []
+
+pealkirjad = doc.select("strong")
 for pealkiri in pealkirjad:
     print(pealkiri.text)
+    
+
+lisainfo = doc.select("small")
+for info in lisainfo:
+    lisainfod.append(info.text)
+    
+hind = doc.select("")
+    
+    
+for i in range(11):
+    toit = {
+        "hind": hinnad[i]
+        "lisainfo": lisainfod[i]
+}
+
+    json_object = json.dumps(toit, indent = 4)
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
+
+# "nimetus": toidunimetus[i]
+#             "hind": hinnad[i]
+
+
