7aaaeed33c74a88158aa07e4210dbf10df7460a0
pingoDeLingo <94634543+pingoDeLingo@users.noreply.github.com>
2022-03-08 13:39:47 +0200

    Add files via upload

--- AastaTegija.py
+++ AastaTegija.py
@@ -8,7 +8,6 @@ url = "http://192.168.22.172/menu-example/"
 sisu = requests.get(url).text
 doc = BeautifulSoup(sisu, "html.parser")
 toidud = []
-toit = {}
 lisainfo_json = []
 hinnad_json = []
 pealkirjad_json = []
@@ -22,9 +21,12 @@ pealkirjad = doc.select("strong")
 for pealkiri in pealkirjad:
     pealkirjad_json.append(pealkiri.text)
     
-nimetused = re.findall('(<h2>)(.*)(<span class="label label-info pull-right">)', tekst)
+    
+nimetused = re.findall('(<h2>)(.*?)(<span class="label label-info pull-right">)', tekst)
 nimetused1 = [nimetus[1] for nimetus in nimetused]
-print(nimetused1)
+for nimetus in nimetused1:
+    nimetused_json.append(nimetus)
+
 
 hinnad = doc.find_all("span")
 for hind in hinnad:
@@ -32,22 +34,21 @@ for hind in hinnad:
     ilushind = ilushind[:-1]
     if ilushind.replace(",", "", 1).isdigit():
         hinnad_json.append(ilushind)
-        
+  
+  
 lisainfo = doc.select("small")
 for info in lisainfo:
     lisainfo_json.append(info.text)
     
     
-# print(pealkirjad_json)
-# print(hinnad_json)
-# print(lisainfo_json)
-
 for i in range(11):
-    toit = {"hind": hinnad_json[i],
+    toit = {
+            "nimetus": nimetused_json[i],
+            "hind": hinnad_json[i],
             "lisainfo": lisainfo_json[i]
+            
 }
 
     json_object = json.dumps(toit, indent=4, separators = (",", ": "))
     with open("sample.json", "a") as outfile:
         outfile.write(json_object)
-    print(json_object)
